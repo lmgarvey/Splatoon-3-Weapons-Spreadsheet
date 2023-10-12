ebd601c5c3782c69df3bd198171e43f240acddde
Lauren <94126547+lmgarvey@users.noreply.github.com>
2023-10-12 15:44:22 -0400

    Added function to fill cell charts
    now fills range as chart, and class specs as chart.
    #TK comments mark where input is currently skipped and points are defaulted, for ease of testing.
    from previous commit, all class specs have been checked and have the correct numbers.
    still need to fix that, after creating the charts successfully, sorting the file in any other way than the default produced way messes them up.

--- main.py
+++ main.py
@@ -55,8 +55,12 @@ def set_up_weapons() -> dict:
     Sets up all the weapons alphabetically within their own classes
     Defaults each weapon to have zero points
     Calls build_[class] for each class in build_weapons.py
-    :return: dict of {class:
-                        {weapon_in_class:
+    :return: dict of
+                    {class:
+                        {
+                        spec A: ( name of class specific A, offset from first spec A column as 0-3 )
+                        spec B: ( name of class specific B, offset from first spec B column as 0-3 )
+                        weapon_in_class:
                             {
                             sub: name of sub
                             special: name of special
@@ -66,6 +70,7 @@ def set_up_weapons() -> dict:
                             weight: light, middle, or heavy
                             class_specific_1: value as number
                             class_specific_2: value as number
+                            special points: points needed for special
                             }
                         }
                     }
@@ -174,7 +179,11 @@ def set_up_sheet(sheet, weapons) -> None:
         class-spec-A, class-spec-B, points for special
     :param sheet: sheet to set up
     :param weapons: {class:
-                        {weapon: points}
+                        {
+                        spec A: (name of class spec A, offset from first spec A column)
+                        spec B: (name of class spec B, offset from first spec B column)
+                        weapon: dict of attribute : value
+                        }
                      }
     :return: None
     """
@@ -195,22 +204,22 @@ def set_up_sheet(sheet, weapons) -> None:
         spec_1_offset = weapons[weapon_class]["spec A"][1]
         spec_2_offset = weapons[weapon_class]["spec B"][1]
 
-        end = len(weapons[weapon_class]) + start - 2    # -2 handles the spec_A and spec_B items
+        end = len(weapons[weapon_class]) + start - 2  # -2 handles the spec_A and spec_B items
         for row in range(start, end):
             # set weapon class in column A
             sheet.cell(row, 1).value = weapon_class
 
             weapon = weapons_dct_as_lst[row - start]
             sheet.cell(row, 2).value = weapon  # column B
-            sheet.cell(row, 3).value = weapons[weapon_class][weapon]["sub"]         # C
-            sheet.cell(row, 4).value = weapons[weapon_class][weapon]["special"]     # D
-            sheet.cell(row, 6).value = weapons[weapon_class][weapon]["points"]      # F
-            sheet.cell(row, 11).value = weapons[weapon_class][weapon]["range"]      # K
-            sheet.cell(row, 13).value = weapons[weapon_class][weapon]["role"]       # M
-            sheet.cell(row, 14).value = weapons[weapon_class][weapon]["weight"]     # N
-            sheet.cell(row, 15 + spec_1_offset).value = weapons[weapon_class][weapon][class_spec_1]     # O, P, Q, R
-            sheet.cell(row, 19 + spec_2_offset).value = weapons[weapon_class][weapon][class_spec_2]     # S, T, U, V
-            sheet.cell(row, 24).value = weapons[weapon_class][weapon]["special points"]     # X
+            sheet.cell(row, 3).value = weapons[weapon_class][weapon]["sub"]  # C
+            sheet.cell(row, 4).value = weapons[weapon_class][weapon]["special"]  # D
+            sheet.cell(row, 6).value = weapons[weapon_class][weapon]["points"]  # F
+            sheet.cell(row, 11).value = weapons[weapon_class][weapon]["range"]  # K
+            sheet.cell(row, 13).value = weapons[weapon_class][weapon]["role"]  # M
+            sheet.cell(row, 14).value = weapons[weapon_class][weapon]["weight"]  # N
+            sheet.cell(row, 15 + spec_1_offset).value = weapons[weapon_class][weapon][class_spec_1]  # O, P, Q, R
+            sheet.cell(row, 19 + spec_2_offset).value = weapons[weapon_class][weapon][class_spec_2]  # S, T, U, V
+            sheet.cell(row, 24).value = weapons[weapon_class][weapon]["special points"]  # X
 
         start = end
 
@@ -281,6 +290,55 @@ def set_up_points_columns(sheet) -> None:
     sheet.conditional_formatting.add('J2:J102', rule)
 
 
+def set_up_cell_charts(sheet) -> None:
+    """
+    Sets up the cell bar charts for range and class specific values:
+        L: range as a chart
+        P: impact as a chart (nominally 'damage (numbers)' column)
+        Q: damage as a chart (nominally 'ink speed (numbers)' column)
+        R: ink speed as a chart (nominally 'charge speed (numbers)' column)
+        S: charge speed as a chart (nominally 'fire rate (numbers)' column)
+        T: fire rate as a chart (nominally 'durability (numbers)' column)
+        U: durability as a chart (nominally 'handling (numbers)' column)
+        V: handling as a chart (nominally 'mobility (numbers)' column)
+        W: mobility as a chart (named accordingly)
+    :param sheet: sheet to set up charts on
+    :return: None
+    """
+    # range's value and chart aren't contiguous with class specs, do it separately
+    for row in range(2, 103):
+        sheet.cell(row, 12).value = "=K{}".format(row)
+    rule = DataBarRule(start_type='num', start_value=0, end_type='num', end_value=100,
+                       color="000000", showValue=False)
+    sheet.conditional_formatting.add('L2:L102', rule)
+
+    # grab values from preceding column
+    read_from_col = ['O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
+    write_to_col =   [15,  16,  17,  18,  19,  20,  21,  22,  23]
+    for i in range(1, len(read_from_col)):
+        # read value from previous column
+        # eg column P (16)'s value comes from column O (15)
+        read_from = read_from_col[i-1]      # previous column we're reading from
+        write_to = write_to_col[i]          # column to put it in
+        check_val_col = write_to_col[i-1]   # for reading value from column-as-number
+        for row in range(2, 103):
+            # check if there IS a value to read from
+            jinkies = sheet.cell(row, check_val_col).value
+            jonkoes = sheet.cell(row, check_val_col - 1).value
+            # if (value to read)   and  (two columns back is empty or is the weight column)
+            if jinkies is not None and (jonkoes is None or type(jonkoes) == str):
+                sheet.cell(row, write_to).value = "={}{}".format(read_from, row)
+
+    # # turn values into charts: hide the numbers, replace with data bar
+    charts_to_add = ['P2:P12', 'Q13:Q17', 'Q34:Q41', 'Q51:Q90', 'R18:R22', 'R42:R50',
+                     'S23:S33', 'S91:S102', 'T2:T12', 'T51:T77', 'U13:U17', 'V18:V22',
+                     'V42:V50', 'V78:V90', 'W23:W41', 'W91:W102']
+    for column in charts_to_add:
+        rule = DataBarRule(start_type='num', start_value=0, end_type='num', end_value=100,
+                           color="000000", showValue=False)
+        sheet.conditional_formatting.add(column, rule)
+
+
 if __name__ == '__main__':
     print("Welcome to `Creating your Splatoon 3 weapons spreadsheet!`\n")
     print()
@@ -293,13 +351,16 @@ if __name__ == '__main__':
     print("At this time, you can choose to (A) fill in your points, or (B) have a spreadsheet created \n"
           "with all of the point values defaulted to zero. If you select (A), you may also type `q` at any\n"
           "time to stop, and have the remaining weapons all defaulted to zero.\n"
-          "Please enter your selection:\n")
+          "Please enter your selection:"
+          " A: Create sheet and fill in your own point values\n"
+          " B: Create sheet and default all points to zero\n"
+          " C: Update an existing sheet (NOT WORKING YET)\n")
     # choice = input()
     choice = "B"  # TK
     while choice.upper() != "A" and choice.upper() != "B":
         print("Sorry, please make sure you type only A or B.\n"
               "A: Fill in your own point values\n"
-              "B: Default all values to zero\n")
+              "B: Default all points to zero\n")
         choice = input()
 
     default_to_zeros = False
@@ -310,24 +371,13 @@ if __name__ == '__main__':
     numbers_sheet = workbook.active
     numbers_sheet.title = "numbers"
 
-    # {class: {weapon : 0} }
     weapons_dct = set_up_weapons()
     collect_points(weapons_dct, default_to_zeros)
     set_up_sheet(numbers_sheet, weapons_dct)
     set_up_points_columns(numbers_sheet)
 
-    # in the future when there's more happening than just points, i would like to do:
-
-    # class:
-    # weapon: sub, special, points, range, role, weight/speed
-
-    # or alternatively,
-    # weapon: {sub: name}, {special: name}, {points: number}, ...
-
-    # blasters:
-    # weapon: impact, fire rate
-    # [class]:
-    # weapon: [class specific 1], [class specific 2]
+    # TK: in progress, charts mess up when you sort by anything other than class:weapon
+    set_up_cell_charts(numbers_sheet)
 
     workbook.save(filename="splatoon_weapons.xlsx")
 
